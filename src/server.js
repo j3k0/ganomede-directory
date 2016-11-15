847f30ce2008631b972b456f65454c6a31e147dd
Jean-Christophe Hoelt <hoelt@fovea.cc>
2016-11-15 10:57:30 +0200

    Improve logging/debugging capabilities

--- src/server.js
+++ src/server.js
@@ -1,14 +1,26 @@
 'use strict';
 
 const restify = require('restify');
+const logger = require('./logger');
 
 module.exports = () => {
   const server = restify.createServer({
-    handleUncaughtExceptions: true
+    handleUncaughtExceptions: true,
+    log: logger
   });
 
   server.use(restify.queryParser());
   server.use(restify.bodyParser());
+
+  // Audit requests
+  server.on('after', restify.auditLogger({ log: logger }));
+
+  // Automatically add a request-id to the response
+  function setRequestId(req, res, next) {
+    res.setHeader('x-request-id', req.id());
+    return next();
+  }
+  server.use(setRequestId);
 
   return server;
 };
