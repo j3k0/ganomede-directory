1e95f63af6dbdde4e0bdeb05902dc1ca3d5e0f8a
Alexey Zabrodsky <elmigranto@gmail.com>
2017-01-20 17:03:12 +0500

    ActionExecutor class for approximating transactions.

---
+++ src/ActionExecutor.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const async = require('async');
+
+// Pass in array of actions, #run() to execute them in series,
+// If one fails, calls action.rollback() on previous ones in reverse order.
+class ActionExecutor {
+  constructor (actions) {
+    this.actions = actions;
+  }
+
+  rollback (reason, rollbackQueue, callback) {
+    async.eachSeries(
+      rollbackQueue,
+      // TODO
+      // rollback() might error, but it should be okay to ignore it for now.
+      // (Also, make sure iterator's cb() is always called with no error.)
+      (action, cb) => action.rollback(cb),
+      (err) => callback(reason)
+    );
+  }
+
+  run (callback) {
+    const rollbackQueue = [];
+
+    async.eachSeries(
+      this.actions,
+      (action, cb) => {
+        action.execute(err => {
+          if (err)
+            return cb(err);
+
+          rollbackQueue.unshift(action);
+          cb();
+        });
+      },
+      (err) => {
+        return err
+          ? this.rollback(err, rollbackQueue, callback)
+          : callback(null);
+      }
+    );
+  }
+}
+
+module.exports = ActionExecutor;
---
+++ tests/ActionExecutor.test.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const ActionExecutor = require('../src/ActionExecutor');
+
+describe('ActionExecutor', () => {
+  describe('#execute()', () => {
+    class TestAction {
+      constructor (resultsRef, num) {
+        this.resultsRef = resultsRef;
+        this.num = num;
+      }
+
+      execute (callback) {
+        this.resultsRef.push(this.num);
+        setImmediate(callback, null);
+      }
+
+      rollback (callback) {
+        this.resultsRef.splice(this.resultsRef.length - 1, 1);
+        setImmediate(callback, null);
+      }
+    }
+
+    class FailingAction {
+      execute (callback) {
+        setImmediate(callback, new Error('FailingAction'));
+      }
+    }
+
+    it('Runs actions one by one', (done) => {
+      const results = [];
+      const executor = new ActionExecutor([
+        new TestAction(results, 1),
+        new TestAction(results, 2),
+        new TestAction(results, 3)
+      ]);
+
+      executor.run((err) => {
+        expect(err).to.be.null;
+        expect(results).to.eql([1, 2, 3]);
+        done();
+      });
+    });
+
+    it('Reverts previous actions if some fail', (done) => {
+      const results = [];
+      const executor = new ActionExecutor([
+        new TestAction(results, 1),
+        new TestAction(results, 2),
+        new FailingAction()
+      ]);
+
+      executor.run((err) => {
+        expect(err).to.be.an('error');
+        expect(results).to.eql([]);
+        done();
+      });
+    });
+  });
+});
