98c5e25548bae80f3ef55f45d981465b99f7bce8
Alexey Zabrodsky <elmigranto@gmail.com>
2017-02-02 18:53:37 +0500

    Rewrote import script to accept single filename and create user from that.

--- src/utils.js
+++ src/utils.js
@@ -2,12 +2,18 @@
 
 const util = require('util');
 
-module.exports = {
-  debugPrint: (what, {json = false} = {}) => {
-    const message = json
-      ? JSON.stringify(what, null, 2)
-      : util.inspect(what, {depth: null, maxArrayLength: null});
+const debugInspect = (what, {json = false} = {}) => {
+  return json
+    ? JSON.stringify(what, null, 2)
+    : util.inspect(what, {depth: null, maxArrayLength: null});
+};
 
-    console.log('%s', message); // eslint-disable-line no-console
-  }
+const debugPrint = (what, {json = false} = {}) => {
+  const message = debugInspect(what, {json});
+  console.log('%s', message); // eslint-disable-line no-console
+};
+
+module.exports = {
+  debugInspect,
+  debugPrint
 };
