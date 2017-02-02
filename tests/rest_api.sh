1b7af9ecf9e4fdcc8e1c6bed53f6a6e3e6cb5d24
Jean-Christophe Hoelt <hoelt@fovea.cc>
2017-02-02 20:50:04 +0200

    Add tests/rest_api.sh
    Full-stack test of the API calls, with app running in docker.

---
+++ tests/rest_api.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+PREFIX=http://localhost:8000/directory/v1
+
+if [ "x$CLEANUP" = "x1" ]; then
+    echo "Cleaning up database"
+    docker-compose stop
+    docker-compose rm -vf
+    docker-compose up -d
+
+    sleep 2
+    echo "Initializing couch views"
+    docker-compose run -e COUCH_SYNC=1 app node index.js | ./node_modules/.bin/bunyan
+fi
+
+set -e
+
+function CURL() {
+    docker-compose up --no-deps --no-recreate -d
+    curl -s -H 'Content-type: application/json' "$@" > .curloutput.txt
+    cat .curloutput.txt | json_pp > .testoutput.txt
+    # testoutput
+}
+
+function outputIncludes() {
+    cat .testoutput.txt | grep "$@" > /dev/null || (echo "      FAIL" && false)
+}
+
+function outputExcludes() {
+    cat .testoutput.txt | grep "$@" > /dev/null || return 0
+    echo "      FAIL" && false
+}
+
+function it() {
+    echo "    - $@"
+}
+
+# Public alias
+it 'exposes the public alias "username"'
+CURL $PREFIX/users -d '{"id":"jeko","password":"12345678","secret":"1234", "aliases":[{"type":"username","public":true,"value":"iamjeko"}]}'
+CURL $PREFIX/users/id/jeko
+outputIncludes username
+
+# Private alias
+it 'keep private aliases hidden'
+CURL $PREFIX/users -d '{"id":"user1","password":"12345678","secret":"1234", "aliases":[{"type":"private","value":"hidden"}]}'
+CURL $PREFIX/users/id/user1
+outputExcludes hidden
+
+# Wrong request
+it 'handles wrong requests'
+CURL $PREFIX/users -d '{"id":"noalias","password":"12345678","secret":"1234"}'
+outputExcludes "InternalError"
+CURL $PREFIX/users/id/noalias
+outputIncludes noalias
